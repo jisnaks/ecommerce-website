e65f332149fbd620fa5a391d7e075cb0c83afffe
jisnaks <123110655+jisnaks@users.noreply.github.com>
2023-10-03 19:32:24 +0530

    Update Footer.js

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -1,25 +1,35 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Footer() {
   return (
 
-    <div className='bg-black  d-flex  justify-content-center aligh-items-center'>
+   <div className='bg-black  d-flex  justify-content-center align-items-center pt-5'>
+      <Container>
+        <Row>
+          <Col xs={12} sm={6} >
+            <div className="px-5  py-2 ">
+              <h2 className='text-xl text-white 300 mb-3'>Locate Us</h2>
+              <p>Ernakulam</p>
+              <p>Mobile: 009683732</p>
+              <p>Phone: 0036352899</p>
+              <p>Email: anime@gmail.com</p>
+            </div>
+          </Col>
+          <Col xs={12} sm={6} > 
+            <div className='p-3 '>
+              <h2 className='text-xl text-white 300 mb-3'>Profile</h2>
+              <p>My Account</p>
+              <p>Checkout</p>
+              <p>Order tracking</p>
+              <p>help & support</p>
+            </div>
+          </Col>
 
-      <div className='d-flex flex-col '>
-        <div className="px-5 me-5 py-2 ">
-          <h2 className='text-xl text-white 300 mb-3'>Locate Us</h2>
-          <p>Ernakulam</p>
-          <p>Mobile: 009683732</p>
-          <p>Phone: 0036352899</p>
-          <p>Email: anime@gmail.com</p>
-        </div>
-        <div className='p-3 ms-5'>
-          <h2 className='text-xl text-white 300 mb-3'>Profile</h2>
-          <p>My Account</p>
-          <p>Checkout</p>
-          <p>Order tracking</p>
-          <p>help & support</p>
-        </div>
-      </div>
+        </Row>
+
+      </Container>
     </div>
   )
 }
